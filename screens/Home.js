c84689744184796dace05cd8d4b64ec369cc7e55
Carlstong <carlstongrefalde@gmail.com>
2022-05-10 12:50:19 +0800

    added Rnative packages

---
+++ screens/Home.js
@@ -0,0 +1,12 @@
+import { Text, View, SafeArea, FlatList } from 'react-native'
+import { useState } from 'react'
+
+import {COLORS, NFTData} from '../constants';
+
+const Home = () => {
+  return (
+     <Text>Home</Text>
+  )
+}
+
+export default Home
